d92f648a01f5c3c873c54a249af5ef52d9c165c6
BethAnn Hanlon <herbivore137@gmail.com>
2019-06-08 18:54:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,15 +6,7 @@ class App < Sinatra::Base
     erb :newteam
   end
   
-  post '/newteam' do
-    # @name = params[:name]
-    # @coach = params[:coach]
-    # @pg = params[:pg]
-    # @sg = params[:sg]
-    # @sf = params[:sf]
-    # @pf = params[:pf]
-    # @c = params[:c]
-    
+  post '/team' do
     @name = params["name"]
     @coach = params["coach"]
     @pg = params["pg"]
